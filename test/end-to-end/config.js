d32216c7239cebf7fb768d63ed9e171e661b50c1
Kris Walker <kris@kixx.name>
2018-03-20 07:39:06 -0400

    Remove old tests
     Changes to be committed:
    	deleted:    test/dynamodb/batch-get-test.js
    	deleted:    test/dynamodb/batch-remove-test.js
    	deleted:    test/dynamodb/batch-set-test.js
    	deleted:    test/dynamodb/get-test.js
    	deleted:    test/dynamodb/remove-test.js
    	deleted:    test/dynamodb/scan-query-test.js
    	deleted:    test/dynamodb/set-test.js
    	deleted:    test/end-to-end/all-test.js
    	deleted:    test/end-to-end/config.js
    	deleted:    test/end-to-end/test-batch-get.js
    	deleted:    test/end-to-end/test-batch-remove.js
    	deleted:    test/end-to-end/test-batch-set.js
    	deleted:    test/end-to-end/test-get.js
    	deleted:    test/end-to-end/test-remove.js
    	deleted:    test/end-to-end/test-scan.js
    	deleted:    test/end-to-end/test-set.js
    	deleted:    test/test-support/create-document.js
    	deleted:    test/test-support/dynalite-server.js
    	deleted:    test/test-support/library.js
    	deleted:    test/test-support/mocks/dynamodb.js
    	deleted:    test/test-support/mocks/provisioned-throughput-exceeded-exception.js
    	deleted:    test/test-support/mocks/resource-not-found-exception.js

--- test/end-to-end/config.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-exports.DYNAMODB_REGION = `us-east-1`;
-exports.DYNAMODB_ENDPOINT = `http://localhost:4567`;
-exports.DYNAMODB_ACCESS_KEY_ID = `foo`;
-exports.DYNAMODB_SECRET_ACCESS_KEY = `bar`;
