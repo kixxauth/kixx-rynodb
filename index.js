51f45092e48ab9ad4578c7542276ce374ea012d9
Kris Walker <kris@kixx.name>
2018-04-12 07:12:12 -0400

    Creating a new entity in DynamoDB
     Changes to be committed:
    	modified:   index.js
    	modified:   lib/dynamodb-client.js
    	modified:   lib/dynamodb.js
    	modified:   lib/entity.js
    	modified:   lib/transaction.js

--- index.js
+++ index.js
@@ -1,4 +1,73 @@
 'use strict';
 
-exports.setupSchema = require(`./lib/setup-schema`);
-exports.transactionFactory = require(`./lib/transaction-factory`);
+const EventEmitter = require('events');
+const Transaction = require('./lib/transaction');
+const DynamoDbClient = require('./lib/dynamodb-client');
+const DynamoDb = require('./lib/dynamodb');
+const {isNonEmptyString} = require('kixx/library');
+
+exports.Transaction = Transaction;
+
+// options.tablePrefix
+// options.awsRegion
+// options.awsAccessKey
+// options.awsSecretKey
+// options.dynamodbEndpoint
+// options.operationTimeout
+// options.backoffMultiplier
+// options.requestTimeout
+exports.create = function create(options = {}) {
+	const emitter = options.emitter || new EventEmitter();
+
+	if (!isNonEmptyString(options.tablePrefix) || !/^[a-zA-Z_]{2,50}$/.test(options.tablePrefix)) {
+		throw new Error(`invalid table prefix String`);
+	}
+	if (!isNonEmptyString(options.awsRegion)) {
+		throw new Error(`invalid awsRegion String`);
+	}
+	if (!isNonEmptyString(options.awsAccessKey)) {
+		throw new Error(`invalid awsAccessKey String`);
+	}
+	if (!isNonEmptyString(options.awsSecretKey)) {
+		throw new Error(`invalid awsSecretKey String`);
+	}
+
+	const {
+		tablePrefix,
+		awsRegion,
+		awsAccessKey,
+		awsSecretKey,
+		dynamodbEndpoint,
+		operationTimeout,
+		backoffMultiplier,
+		requestTimeout
+	} = options;
+
+	const client = new DynamoDbClient({
+		emitter,
+		awsRegion,
+		awsAccessKey,
+		awsSecretKey,
+		dynamodbEndpoint,
+		operationTimeout,
+		backoffMultiplier,
+		requestTimeout
+	});
+
+	const dynamodb = new DynamoDb({
+		emitter,
+		tablePrefix,
+		client
+	});
+
+	return Object.assign(emitter, {
+		createTransaction() {
+			return new Transaction({dynamodb});
+		},
+
+		setupSchema() {
+		},
+
+		dynamodb
+	});
+};
