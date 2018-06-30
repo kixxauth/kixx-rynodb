955e792f7a53d4603941a86ef88ed77c39332818
Kris Walker <kris@kixx.name>
2018-06-30 07:17:06 -0400

    Error testing for #updateOrCreate()

--- test/transaction/update-or-create-item-test.js
+++ test/transaction/update-or-create-item-test.js
@@ -116,7 +116,7 @@ module.exports = function (t) {
 		sinon.stub(dynamodb.client, '_request').callsFake(function (target) {
 			switch (target) {
 			case 'GetItem':
-				return Promise.resolve({entity: {}});
+				return Promise.resolve({Item: {}});
 			case 'PutItem':
 				return Promise.reject(new Error('test error'));
 			default:
@@ -143,6 +143,181 @@ module.exports = function (t) {
 
 		t.it('rejects with the expected exception', () => {
 			assert.isEqual('Error during Transaction#updateOrCreateItem(): Error in DynamoDB#setEntity(): test error', error.message);
+		});
+	});
+
+	t.describe('with error in query() during update', (t) => {
+		const emitter = new EventEmitter();
+
+		const dynamodb = DynamoDb.create({
+			emitter,
+			awsRegion,
+			awsAccessKey,
+			awsSecretKey,
+			tablePrefix
+		});
+
+		const txn = Transaction.create({dynamodb});
+
+		sinon.stub(dynamodb.client, '_request').callsFake(function (target) {
+			switch (target) {
+			case 'GetItem':
+				return Promise.resolve({Item: {}});
+			case 'PutItem':
+				return Promise.resolve({});
+			case 'Query':
+				return Promise.reject(new Error('test error'));
+			default:
+				return Promise.reject(new Error(`unexpected _request() target: '${target}'`));
+			}
+		});
+
+		const object = {
+			scope: 'some_scope',
+			type: 'some_type',
+			id: 'some_id'
+		};
+
+		let error;
+
+
+		t.before(function (done) {
+			txn.updateOrCreateItem(object).then(function () {
+				throw new Error('should not have resolved');
+			}).catch(function (err) {
+				error = err;
+				return null;
+			}).then(done);
+		});
+
+		t.it('rejects with the expected exception', () => {
+			assert.isEqual('Error during Transaction#updateOrCreateItem(): Error in DynamoDB#getIndexEntries(): test error', error.message);
+		});
+	});
+
+	t.describe('with error in BatchWriteItem during create', (t) => {
+		const emitter = new EventEmitter();
+
+		const dynamodb = DynamoDb.create({
+			emitter,
+			awsRegion,
+			awsAccessKey,
+			awsSecretKey,
+			tablePrefix
+		});
+
+		const indexes = {
+			some_type: {
+				byTitle(item, emit) {
+					emit(item.attributes.title);
+				}
+			}
+		};
+
+		const txn = Transaction.create({dynamodb, indexes});
+
+		sinon.stub(dynamodb.client, '_request').callsFake(function (target) {
+			switch (target) {
+			case 'GetItem':
+				return Promise.resolve({});
+			case 'PutItem':
+				return Promise.resolve({});
+			case 'BatchWriteItem':
+				return Promise.reject(new Error('test error'));
+			default:
+				return Promise.reject(new Error(`unexpected _request() target: '${target}'`));
+			}
+		});
+
+		const object = {
+			scope: 'some_scope',
+			type: 'some_type',
+			id: 'some_id',
+			attributes: {
+				title: 'Foo'
+			}
+		};
+
+		let error;
+
+		t.before(function (done) {
+			txn.updateOrCreateItem(object).then(function () {
+				throw new Error('should not have resolved');
+			}).catch(function (err) {
+				error = err;
+				return null;
+			}).then(done);
+		});
+
+		t.it('rejects with the expected exception', () => {
+			assert.isEqual('Error during Transaction#updateOrCreateItem(): Error in DynamoDB#updateIndexEntries(): test error', error.message);
+		});
+	});
+
+	t.describe('with error in BatchWriteItem during update', (t) => {
+		const emitter = new EventEmitter();
+
+		const dynamodb = DynamoDb.create({
+			emitter,
+			awsRegion,
+			awsAccessKey,
+			awsSecretKey,
+			tablePrefix
+		});
+
+		const indexes = {
+			some_type: {
+				byTitle(item, emit) {
+					emit(item.attributes.title);
+				}
+			}
+		};
+
+		const txn = Transaction.create({dynamodb, indexes});
+
+		sinon.stub(dynamodb.client, '_request').callsFake(function (target) {
+			switch (target) {
+			case 'GetItem':
+				return Promise.resolve({Item: {
+					_scope: {S: 'some_scope'},
+					_type: {S: 'some_type'},
+					_id: {S: 'some_id'},
+					_created: {S: new Date().toISOString()},
+					_updated: {S: new Date().toISOString()}
+				}});
+			case 'PutItem':
+				return Promise.resolve({});
+			case 'Query':
+				return Promise.resolve({Items: []});
+			case 'BatchWriteItem':
+				return Promise.reject(new Error('test error'));
+			default:
+				return Promise.reject(new Error(`unexpected _request() target: '${target}'`));
+			}
+		});
+
+		const object = {
+			scope: 'some_scope',
+			type: 'some_type',
+			id: 'some_id',
+			attributes: {
+				title: 'Foo'
+			}
+		};
+
+		let error;
+
+		t.before(function (done) {
+			txn.updateOrCreateItem(object).then(function () {
+				throw new Error('should not have resolved');
+			}).catch(function (err) {
+				error = err;
+				return null;
+			}).then(done);
+		});
+
+		t.it('rejects with the expected exception', () => {
+			assert.isEqual('Error during Transaction#updateOrCreateItem(): Error in DynamoDB#updateIndexEntries(): test error', error.message);
 		});
 	});
 };
