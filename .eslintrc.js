e89357b4948ffded799f79aeea2247412e63c966
Kris Walker <kris@kixx.name>
2020-02-25 22:24:42 -0500

    Update linting rules.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
 	},
 	"extends": "eslint:recommended",
 	"parserOptions": {
-		"ecmaVersion": 2017
+		"ecmaVersion": 2015
 	},
 	"rules": {
 		"array-bracket-spacing": [
@@ -16,9 +16,34 @@ module.exports = {
 		"array-callback-return": [
 			"error"
 		],
+		"arrow-body-style": [
+			"error",
+			"always"
+		],
 		"arrow-parens": [
 			"error",
 			"always"
+		],
+		"arrow-spacing": [
+			"error"
+		],
+		"block-spacing": [
+			"error"
+		],
+		"brace-style": [
+			"error",
+			"stroustrup"
+		],
+		"callback-return": [
+			"error"
+		],
+		"camelcase": [
+			"error"
+		],
+		"capitalized-comments": [
+			"error",
+			"always",
+			{ ignoreConsecutiveComments: true }
 		],
 		"comma-dangle": [
 			"error",
@@ -30,19 +55,40 @@ module.exports = {
 				exports: "never"
 			}
 		],
+		"comma-spacing": [
+			"error"
+		],
+		"comma-style": [
+			"error"
+		],
 		"curly": [
 			"error"
 		],
 		"eol-last": [
 			"error"
 		],
+		"eqeqeq": [
+			"error"
+		],
 		"indent": [
 			"error",
-			"tab"
+			"tab",
+			{ SwitchCase: 1 }
+		],
+		"keyword-spacing": [
+			"error"
 		],
 		"linebreak-style": [
 			"error",
 			"unix"
+		],
+		"max-len": [
+			"error",
+			{ tabWidth: 2, code: 120, comments: 100 }
+		],
+		"multiline-ternary": [
+			"error",
+			"always-multiline"
 		],
 		"no-buffer-constructor": [
 			"error"
@@ -53,22 +99,78 @@ module.exports = {
 		"no-console": [
 			"error"
 		],
+		"no-else-return": [
+			"error"
+		],
+		"no-eval": [
+			"error"
+		],
+		"no-extend-native": [
+			"error"
+		],
 		"no-floating-decimal": [
+			"error"
+		],
+		"no-implicit-coercion": [
+			"error"
+		],
+		"no-implied-eval": [
+			"error"
+		],
+		"no-multi-assign": [
 			"error"
 		],
 		"no-multi-spaces": [
 			"error"
 		],
+		"no-nested-ternary": [
+			"error"
+		],
+		"no-new-wrappers": [
+			"error"
+		],
 		"no-path-concat": [
+			"error"
+		],
+		"no-plusplus": [
 			"error"
 		],
 		"no-process-env": [
 			"error"
 		],
+		"no-return-assign": [
+			"error"
+		],
+		// "no-restricted-syntax": [
+		// 	"error",
+		// 	""
+		// ],
+		"no-sequences": [
+			"error"
+		],
+		"no-shadow": [
+			"error",
+			{ builtinGlobals: true, hoist: "all" }
+		],
 		"no-shadow-restricted-names": [
 			"error"
 		],
 		"no-template-curly-in-string": [
+			"error"
+		],
+		"no-throw-literal": [
+			"error"
+		],
+		"no-trailing-spaces": [
+			"error"
+		],
+		"no-underscore-dangle": [
+			"error"
+		],
+		"no-unmodified-loop-condition": [
+			"error"
+		],
+		"no-unused-expressions": [
 			"error"
 		],
 		"no-use-before-define": [
@@ -82,16 +184,34 @@ module.exports = {
 			"warn",
 			{ location: "anywhere" }
 		],
+		"no-whitespace-before-property": [
+			"error"
+		],
 		"object-curly-spacing": [
 			"error",
 			"always"
 		],
+		"operator-assignment": [
+			"error",
+			"never"
+		],
+		"operator-linebreak": [
+			"error",
+			"before"
+		],
 		"prefer-const": [
+			"error"
+		],
+		"prefer-destructuring": [
+			"error"
+		],
+		"prefer-promise-reject-errors": [
 			"error"
 		],
 		"quotes": [
 			"error",
-			"single"
+			"single",
+			{ avoidEscape: true }
 		],
 		"radix": [
 			"error"
@@ -99,6 +219,22 @@ module.exports = {
 		"semi": [
 			"error",
 			"always"
+		],
+		"spaced-comment": [
+			"error"
+		],
+		"space-before-blocks": [
+			"error"
+		],
+		"space-before-function-paren": [
+			"error",
+			{ anonymous: "always", named: "never", asyncArrow: "never" }
+		],
+		"space-in-parens": [
+			"error"
+		],
+		"space-infix-ops": [
+			"error"
 		],
 		"strict": [
 			"error"
